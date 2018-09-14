f01e5cd40781593787d8847933fa8778fcfd95ab
Angie1508 <43172283+Angie1508@users.noreply.github.com>
2018-09-14 15:06:55 +0200

    Add files via upload
    inital add drag and drop

---
+++ spring-joke-app/src/main/java/com/anamarijapoljak/springjokeapp/SpringjokeappApplication.java
@@ -0,0 +1,12 @@
+package com.anamarijapoljak.springjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjokeappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjokeappApplication.class, args);
+    }
+}
